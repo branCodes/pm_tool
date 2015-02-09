0be596cfe1778931e8b38e72366ef0588ed923e8
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-02-08 17:26:57 -0800

    favourites working properly

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -10,7 +10,7 @@ class FavouritesController < ApplicationController
   @favourite = @project.favourites.new
   @favourite.user = current_user
     if @favourite.save 
-      redirect_to @project, notice: "You have unfavourited this project!"
+      redirect_to @project, notice: "You have favourited this project!"
     else
       redirect_to @project, alert: "Error! You were unable to favourite this project!"
     end
@@ -19,7 +19,7 @@ class FavouritesController < ApplicationController
   def destroy 
     @favourite = current_user.favourites.find(params[:id])
     if @favourite.destroy
-      redirect_to @project, notice: "You have favourited this project!"
+      redirect_to @project, notice: "You have unfavourited this project!"
     else
       redirect_to @project, alert: "Error! Unable to unfavourite this project!"
     end
