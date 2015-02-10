8b8a3ce611cc55029ff9c7f596cd48d93089d12e
Brandyn Prasad <brandyn.prasad@gmail.com>
2015-02-09 16:50:33 -0800

    comment create ajax working

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController 
+  respond_to :js
 
   def index
     @discussion = Discussion.find params[:discussion_id]
@@ -9,17 +10,21 @@ class CommentsController < ApplicationController
   end
 
   def create
+    @project = Project.find params[:project_id]
     @discussion = Discussion.find params[:discussion_id]
     @comment = @discussion.comments.create(comment_params)
     @comment.user = current_user
     # @comment.discussion_id = @discussion.id
     if @comment.save && @discussion.user != current_user
+      respond_with ()
       DiscussionMailer.delay.notify_discussion_owner(@comment)
-      redirect_to project_discussion_path(@discussion.project, @discussion), notice: "Comment Created!"
+      #redirect_to project_discussion_path(@discussion.project, @discussion), notice: "Comment Created!"
     elsif @comment.save
-      redirect_to project_discussion_path(@discussion.project, @discussion), notice: "Comment Created!"
+      respond_with ()
+      #redirect_to project_discussion_path(@discussion.project, @discussion), notice: "Comment Created!"
     else
-      redirect_to project_discussion_path(@discussion.project, @discussion), notice: "Comments cannot be blank!"
+      #redirect_to project_discussion_path(@discussion.project, @discussion), notice: "Comments cannot be blank!"
+      respond_with ()
     end
   end
 
